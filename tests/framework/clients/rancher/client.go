f554b4a7e6fe89d32807c6226a67bbb932380f42
igomez06 <israel.gomez@suse.com>
2021-11-12 16:12:55 -0700

    Added rancher client, config, and dynamic client.

---
+++ tests/framework/clients/rancher/client.go
@@ -0,0 +1,50 @@
+package rancher
+
+import (
+	"fmt"
+
+	management "github.com/rancher/rancher/tests/framework/clients/rancher/generated/management/v3"
+	"github.com/rancher/rancher/tests/framework/pkg/clientbase"
+	"github.com/rancher/rancher/tests/framework/pkg/session"
+	"k8s.io/client-go/rest"
+)
+
+type Client struct {
+	Management    *management.Client
+	RancherConfig *Config
+}
+
+func NewClient(bearerToken string, rancherConfig *Config, session *session.Session) (*Client, error) {
+	c := &Client{
+		RancherConfig: rancherConfig,
+	}
+
+	var err error
+	c.Management, err = management.NewClient(clientOpts(newRestConfig(bearerToken, rancherConfig), c.RancherConfig))
+	if err != nil {
+		return nil, err
+	}
+	c.Management.APIBaseClient.Ops.Session = session
+
+	return c, nil
+}
+
+func newRestConfig(bearerToken string, rancherConfig *Config) *rest.Config {
+	return &rest.Config{
+		Host:        rancherConfig.RancherHost,
+		BearerToken: bearerToken,
+		TLSClientConfig: rest.TLSClientConfig{
+			Insecure: *rancherConfig.Insecure,
+			CAFile:   rancherConfig.CAFile,
+		},
+	}
+}
+
+func clientOpts(restConfig *rest.Config, rancherConfig *Config) *clientbase.ClientOpts {
+	return &clientbase.ClientOpts{
+		URL:      fmt.Sprintf("https://%s/v3", rancherConfig.RancherHost),
+		TokenKey: restConfig.BearerToken,
+		Insecure: restConfig.Insecure,
+		CACerts:  rancherConfig.CACerts,
+	}
+}
