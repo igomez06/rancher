14130c3cd52efc51ef7ea01ec6fd00351e411898
igomez06 <israel.gomez@suse.com>
2021-11-18 12:26:55 -0700

    Method stubs for our watch package.

--- tests/framework/pkg/wait/wait.go
+++ tests/framework/pkg/wait/wait.go
@@ -1,1 +1,10 @@
 package wait
+
+type CheckFunc func() (ready bool, err error)
+
+func wait(check CheckFunc) error {
+	// load timeout values and such from config
+	// call check func on a ticker
+	// if error return error, if ready return nil
+	return nil
+}
--- tests/framework/pkg/wait/watch.go
+++ tests/framework/pkg/wait/watch.go
@@ -1,1 +1,9 @@
 package wait
+
+import (
+	"k8s.io/apimachinery/pkg/watch"
+)
+
+func WatchWait(watchInterface watch.Interface, check CheckFunc) error {
+	return nil
+}
