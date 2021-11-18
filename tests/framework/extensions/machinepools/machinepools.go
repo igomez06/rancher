c8e280a9aa492f9d33bd302484320a922f32d918
igomez06 <israel.gomez@suse.com>
2021-11-18 10:54:20 -0700

    Made fixes, and added new file for amazonec2configs machine config.

--- tests/framework/extensions/machinepools/machinepools.go
+++ tests/framework/extensions/machinepools/machinepools.go
@@ -11,34 +11,7 @@ import (
 	"k8s.io/client-go/dynamic"
 )
 
-const (
-	AWSKind           = "Amazonec2Config"
-	AWSPoolType       = "rke-machine-config.cattle.io.amazonec2config"
-	AWSResourceConfig = "amazonec2configs"
-)
-
-func NewAWSMachineConfig(generatedPoolName, namespace, region string) *unstructured.Unstructured {
-	machineConfig := &unstructured.Unstructured{}
-	machineConfig.SetAPIVersion("rke-machine-config.cattle.io/v1")
-	machineConfig.SetKind(AWSKind)
-	machineConfig.SetGenerateName(generatedPoolName)
-	machineConfig.SetNamespace(namespace)
-	machineConfig.Object["region"] = region
-	machineConfig.Object["instanceType"] = "t3a.medium"
-	machineConfig.Object["sshUser"] = "ubuntu"
-	machineConfig.Object["type"] = AWSPoolType
-	machineConfig.Object["vpcId"] = "vpc-bfccf4d7"
-	machineConfig.Object["volumeType"] = "gp2"
-	machineConfig.Object["zone"] = "a"
-	machineConfig.Object["retries"] = "5"
-	machineConfig.Object["rootSize"] = "16"
-	machineConfig.Object["securityGroup"] = []string{
-		"rancher-nodes",
-	}
-
-	return machineConfig
-}
-
+// CreateMachineConfig is a helper method that actually creates the rke-machine-config
 func CreateMachineConfig(resource string, machinePoolConfig *unstructured.Unstructured, client dynamic.Interface) (*unstructured.Unstructured, error) {
 	groupVersionResource := schema.GroupVersionResource{
 		Group:    "rke-machine-config.cattle.io",
@@ -46,12 +19,11 @@ func CreateMachineConfig(resource string, machinePoolConfig *unstructured.Unstru
 		Resource: resource,
 	}
 
-	ctx := context.Background()
-	podResult, err := client.Resource(groupVersionResource).Namespace(machinePoolConfig.GetNamespace()).Create(ctx, machinePoolConfig, metav1.CreateOptions{})
+	podResult, err := client.Resource(groupVersionResource).Namespace(machinePoolConfig.GetNamespace()).Create(context.TODO(), machinePoolConfig, metav1.CreateOptions{})
 	return podResult, err
 }
 
-func MachinePoolSetup(controlPlaneRole, etcdRole, workerRole bool, poolName string, quantity int32, machineConfig *unstructured.Unstructured) apisV1.RKEMachinePool {
+func NewRKEMachinePool(controlPlaneRole, etcdRole, workerRole bool, poolName string, quantity int32, machineConfig *unstructured.Unstructured) apisV1.RKEMachinePool {
 	machineConfigRef := &corev1.ObjectReference{
 		Kind: machineConfig.GetKind(),
 		Name: machineConfig.GetName(),
