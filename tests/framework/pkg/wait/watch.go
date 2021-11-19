e1a5f4ea5ee31928240cba86c6ac23a68a76f2bd
igomez06 <israel.gomez@suse.com>
2021-11-19 11:12:01 -0700

    Implementation for our wait package.

--- tests/framework/pkg/wait/watch.go
+++ tests/framework/pkg/wait/watch.go
@@ -1,9 +1,36 @@
 package wait
 
 import (
+	"fmt"
+
 	"k8s.io/apimachinery/pkg/watch"
 )
 
 func WatchWait(watchInterface watch.Interface, check CheckFunc) error {
-	return nil
+	defer func() {
+		watchInterface.Stop()
+	}()
+
+	for {
+		select {
+		case event, open := <-watchInterface.ResultChan():
+			if !open {
+				return fmt.Errorf("timeout waiting on condition")
+			}
+			switch event.Type {
+			case watch.Modified:
+				err := wait(check)
+				if err != nil {
+					return err
+				}
+				return nil
+			case watch.Deleted:
+				err := wait(check)
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+		}
+	}
 }
